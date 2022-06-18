a65238edda6096d9700e221fc06f90d4f39f97a9
Rich Tong <1782087+richtong@users.noreply.github.com>
2022-06-18 16:44:43 -0700

    fix: Add xeyes, linted Dockerfile
    Adding xeyes to test X11 connection
    Dockerfile.rich is a hadolint clean and optimized build
    Makefile gets ORG, IMAGE and DOCKEFILE parameters

--- Makefile
+++ Makefile
@@ -1,7 +1,23 @@
-# Makefile for richtong/lib
-#
+## Makefile for richtong/lib
+##
+
 # Release tag
-TAG=0.9
+TAG ?= 0.9
+
+# using github container registery
+ORG ?= ghcr.io/netdrones
+IMAGE ?= orbslam3
+DOCKEFILE ?= Dockerfile
+
+## build: creates the build container
+.PHONY: build
+build:
+	ORG="$(ORG)" IMAGE="$(IMAGE)" DOCKERFILE="$(DOCKERFILE)" ./build_image.sh
+
+## container: starts build container to create artifacts in host
+.PHONY: container
+container:
+	ORG="$(ORG)" IMAGE="$(IMAGE)" ./build_container_rich.sh
 
 ## test: test the library
 .PHONY: test
@@ -15,6 +31,5 @@ clean:
 
 ## all: build all
 .PHONY: all
-
 
 include ./lib/include.mk
--- build_image.sh
+++ build_image.sh
@@ -5,14 +5,16 @@ SCRIPT_DIR="${SCRIPT_DIR:-"$(cd "$(dirname "${BASH_SOURCE[0]}")" && pwd -P)"}"
 # default for docker hub if you want to use github container registry add
 # ORG="${ORG:-ghcr.io/netdrones}"
 ORG="${ORG:-jahaniam}"
+DOCKERFILE="${DOCKERFILE:-Dockerfile}"
+IMAGE="${IMAGE:-orbslam3}"
 
 #shellcheck disable=SC2086
-echo $SCRIPTNAME: docker build -t "$ORG/orbslam3" .
+echo $SCRIPTNAME: docker build -t "$ORG/$IMAGE" .
 
 if [[ $OSTYPE =~ darwin ]]; then
 	echo "$SCRIPTNAME: OSX using buildx for multiplatform and will push"
-	docker buildx build --push --platform linux/amd64,linux/arm64 -t "$ORG/orbslam3" .
+	docker buildx build --push --platform linux/amd64,linux/arm64 -t "$ORG/$IMAGE" -f "$DOCKERFILE" .
 else
 	# shellcheck disable=SC2086
-	docker build -t "$ORG/orbslam3" .
+	docker build -t "$ORG/$IMAGE" -f "$DOCKERFILE".
 fi
